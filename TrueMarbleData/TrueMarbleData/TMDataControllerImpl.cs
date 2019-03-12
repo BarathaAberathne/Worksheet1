887106e2a832d1b3625bd97b91eb3c742bac0318
BarathaAberathne <barathaaberathne@gmail.com>
2019-03-12 21:55:48 +0530

    tenpm

---
+++ TrueMarbleData/TrueMarbleData/TMDataControllerImpl.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ServiceModel;
+using TrueMarbleLibrary;
+namespace TrueMarbleData
+{
+
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple,UseSynchronizationContext = false,InstanceContextMode = InstanceContextMode.Single)])]
+    internal class TMDataControllerImpl : ITMDataController
+    {
+        int width;
+        int height;
+        int x;
+        int y;
+
+        public TMDataControllerImpl()
+        {
+        }
+        public int GetTileWidth()
+        {
+            int width;
+            int height;
+            TrueMarble.GetTileSize(out width, out height);
+            return width;
+        }
+
+        public int GetTileHeight()
+        {
+            int width;
+            int height;
+            TrueMarble.GetTileSize(out width, out height);
+            return height;
+        }
+
+        public int GetNumTilesAcross(int zoom) {
+            TrueMarble.GetNumTiles( zoom, out x, out y);
+            return x;
+
+        }
+
+        public int GetNumTilesDown(int zoom)
+        {
+            int x;
+            int y;
+            TrueMarble.GetNumTiles(zoom, out x, out y);
+            return y;
+        }
+
+        public byte[] LoadTile(int zoom, int x, int y)
+        {
+
+        }
+    }
+}
